36dc83f5af7df0fd4a9e2419955ab26b46c0f395
Nick Strayer <nick.strayer@gmail.com>
2020-03-05 13:49:09 -0600

    Basic tests of uni, bi, and tri-partite network node loading

--- src/test-Node_Container.cpp
+++ src/test-Node_Container.cpp
@@ -54,6 +54,31 @@ context("Basic loading bipartite network") {
 
     expect_true(nodes.get_nodes_of_type(0).start_index == 0);
     expect_true(nodes.get_nodes_of_type(0).size == 2);
+    expect_true(nodes.get_nodes_of_type(1).start_index == 2);
+    expect_true(nodes.get_nodes_of_type(1).size == 2);
+  }
+}
+
+
+context("Basic loading tripartite network") {
+
+  const Rcpp::CharacterVector nodes_id{"a1", "a2", "b1", "c1", "c2"};
+  const Rcpp::CharacterVector nodes_type{"a", "a", "b", "c", "c"};
+  const Rcpp::CharacterVector types_name{"a", "b", "c"};
+
+  test_that("Sizing is correct") {
+    expect_true(Node_Container(nodes_id, nodes_type, types_name).size() == 5);
+  }
+
+  test_that("Type info is correct") {
+    auto nodes = Node_Container(nodes_id, nodes_type, types_name);
+
+    expect_true(nodes.get_nodes_of_type(0).start_index == 0);
+    expect_true(nodes.get_nodes_of_type(0).size == 2);
+    expect_true(nodes.get_nodes_of_type(1).start_index == 2);
+    expect_true(nodes.get_nodes_of_type(1).size == 1);
+    expect_true(nodes.get_nodes_of_type(2).start_index == 3);
+    expect_true(nodes.get_nodes_of_type(2).size == 2);
   }
 
 

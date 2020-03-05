5c1a89631f5b93fda0436a772812686806a0bb8d
Nick Strayer <nick.strayer@gmail.com>
2020-03-05 15:28:23 -0600

    Edge addition now is smart about edge types and undeclared nodes.

--- src/Edge_Container.h
+++ src/Edge_Container.h
@@ -24,36 +24,65 @@ public:
                  const CharacterVector& edges_to,
                  const CharacterVector& nodes_id,
                  Node_Container &nodes){
+
     // We need to quickly go from a node string id to its integer index in nodes_* vectors
     std::unordered_map<string, Node&> node_id_to_index;
     for (int i = 0; i < nodes_id.size(); i++) {
       node_id_to_index.emplace(nodes_id[i], nodes.at(i));
     }
 
+    auto get_node = [&](const string& node_id, const int edge_index) {
+      const auto node_loc = node_id_to_index.find(node_id);
+      if (node_loc == node_id_to_index.end()) {
+        stop("Node " + node_id + " from edges "
+               + string(edges_from[edge_index]) + " - " + string(edges_to[edge_index])
+               + " was not provided in list of nodes");
+      }
+      return &(node_loc->second);
+    };
+
+    const bool multipartite_nodes = nodes.is_multipartite();
+
     for (int i = 0; i < edges_from.size(); i++) {
-      Node& from_node = node_id_to_index.at(string(edges_from[i]));
-      Node& to_node = node_id_to_index.at(string(edges_to[i]));
 
-      // Get edge type for this edge
-      const auto edge_type = Edge_Type(from_node.type_index, to_node.type_index);
+      Node * from_node = get_node(string(edges_from[i]), i);
+      Node * to_node   = get_node(string(edges_to[i]), i);
 
-      // Either check it's allowed or add it to seen types
-      if (types_specified) {
-        if (edge_types.count(edge_type) == 0){
-          stop("The edge type from edge "
-                 + string(edges_from[i]) + " - "
-                 + string(edges_to[i])
-                 + " was not specified in allowed edge types");
+      // We only need to check edge types if we have multiple node types
+      if (multipartite_nodes) {
+        // Get edge type for this edge
+        const auto edge_type = Edge_Type(from_node->type_index, to_node->type_index);
+
+        const bool not_in_edge_types = edge_types.count(edge_type) == 0;
+
+        if (edge_type.is_matching()){
+          stop("Error for edge " + string(edges_from[i]) + " - " + string(edges_to[i])
+                 + ": Can't have an edge between two nodes of the same type in multipartite networks");
         }
-      } else {
-        edge_types.insert(edge_type);
+
+        // Only need to take action if this is a new edge type
+        if (not_in_edge_types) {
+          if (types_specified) {
+            stop("The edge type from edge "+ string(edges_from[i]) + " - "
+                 + string(edges_to[i]) + " was not specified in allowed edge types");
+          } else {
+            // Make sure that this edge doesn't violate the rules of multipartite edges of being between nodes of the same type
+            edge_types.insert(edge_type);
+          }
+        }
       }
 
-      edges.emplace_back(from_node.index, to_node.index);
+
+      edges.emplace_back(from_node->index, to_node->index);
 
       // Add edges to nodes as well
-      from_node.add_edge(to_node.index);
-      to_node.add_edge(from_node.index);
+      from_node->add_edge(to_node->index);
+      to_node->add_edge(from_node->index);
+    }
+
+    // If we're in a unipartite network we never filled out the edge types, so do that here.
+    if (!multipartite_nodes) {
+      edge_types.insert(Edge_Type(0, 0));
     }
 
   }
@@ -63,6 +92,8 @@ public:
     return edges.size();
   }
 };
+
+
 // ## `Edge_Container::`
 // __Data__
 // `vec<Edge>      edges`: Vector containing `Edges` that mirror order of `edges_*` vectors

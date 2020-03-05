a2f5d972cb458eca42f050c5ca8ff23b4dbaf201
Nick Strayer <nick.strayer@gmail.com>
2020-03-05 12:00:57 -0600

    Expanded Ordered_Pair test to strings as well

--- src/test-ordered_pair.cpp
+++ src/test-ordered_pair.cpp
@@ -13,20 +13,22 @@
 #include <testthat.h>
 #include "Ordered_Pair.h"
 
+using Int_Pair = Ordered_Pair<int>;
+using String_Pair = Ordered_Pair<std::string>;
 
 // Initialize a unit test context. This is similar to how you
 // might begin an R test file with 'context()', expect the
 // associated context should be wrapped in braced.
-context("Ordered_Pair template works properly") {
+context("Ordered_Pair template works properly with integers") {
 
-  const auto p12 = Ordered_Pair<int>(1,2);
-  const auto p22 = Ordered_Pair<int>(2,2);
-  const auto p23 = Ordered_Pair<int>(3,2);
-  const auto p13 = Ordered_Pair<int>(3,1);
+  const auto p12 = Int_Pair(1,2);
+  const auto p22 = Int_Pair(2,2);
+  const auto p23 = Int_Pair(3,2);
+  const auto p13 = Int_Pair(3,1);
 
   test_that("Equality works") {
-    expect_true(p12 == Ordered_Pair<int>(2,1));
-    expect_true(p12 == Ordered_Pair<int>(1,2));
+    expect_true(p12 == Int_Pair(2,1));
+    expect_true(p12 == Int_Pair(1,2));
     expect_false(p12 == p22);
     expect_false(p12 == p13);
   }
@@ -41,16 +43,57 @@ context("Ordered_Pair template works properly") {
 
 }
 
-context("Hashing works properly") {
+context("Ordered_Pair template works properly with strings") {
 
-  std::unordered_set<Ordered_Pair<int>, Ordered_Pair_Hash<int>> pair_set;
+  const auto p_ab = String_Pair("a","b");
+  const auto p_bb = String_Pair("b","b");
+  const auto p_cb = String_Pair("c","b");
+  const auto p_ad = String_Pair("a","d");
+
+  test_that("Equality works") {
+    expect_true(p_ab == String_Pair("b","a"));
+    expect_true(p_cb == String_Pair("c", "b"));
+    expect_false(p_ab == p_bb);
+    expect_false(p_ab == p_ad);
+  }
+
+  test_that("Less Than works"){
+    expect_true(p_ab < p_bb);
+    expect_true(p_ad < p_bb);
+
+    expect_false(p_bb < p_ab);
+    expect_false(p_bb < p_ad);
+  }
+
+}
+
+context("Hashing works properly with ints") {
+
+  std::unordered_set<Int_Pair, Ordered_Pair_Hash<int>> pair_set;
 
 
-  pair_set.insert(Ordered_Pair<int>(1,2));
-  pair_set.insert(Ordered_Pair<int>(2,1));
-  pair_set.insert(Ordered_Pair<int>(2,2));
-  pair_set.insert(Ordered_Pair<int>(3,2));
-  pair_set.insert(Ordered_Pair<int>(3,1));
+  pair_set.insert(Int_Pair(1,2));
+  pair_set.insert(Int_Pair(2,1));
+  pair_set.insert(Int_Pair(2,2));
+  pair_set.insert(Int_Pair(3,2));
+  pair_set.insert(Int_Pair(3,1));
+
+  test_that("Size is right") {
+    expect_true(pair_set.size() == 4); // One duplicate added
+  }
+
+}
+
+context("Hashing works properly with strings") {
+
+  std::unordered_set<String_Pair, Ordered_Pair_Hash<std::string>> pair_set;
+
+
+  pair_set.insert(String_Pair("a","b"));
+  pair_set.insert(String_Pair("b","a"));
+  pair_set.insert(String_Pair("b","b"));
+  pair_set.insert(String_Pair("c","b"));
+  pair_set.insert(String_Pair("c","a"));
 
   test_that("Size is right") {
     expect_true(pair_set.size() == 4); // One duplicate added

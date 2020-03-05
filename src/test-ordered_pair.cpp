edc4e3556aaa967b60a10b10febd853f06546438
Nick Strayer <nick.strayer@gmail.com>
2020-03-05 11:39:39 -0600

    Added c++ units tests using the catch library thanks to testthat::use_catch()

---
+++ src/test-ordered_pair.cpp
@@ -0,0 +1,41 @@
+/*
+ * This file uses the Catch unit testing library, alongside
+ * testthat's simple bindings, to test a C++ function.
+ *
+ * For your own packages, ensure that your test files are
+ * placed within the `src/` folder, and that you include
+ * `LinkingTo: testthat` within your DESCRIPTION file.
+ */
+
+// All test files should include the <testthat.h>
+// header file.
+#include <testthat.h>
+#include "Ordered_Pair.h"
+
+
+// Initialize a unit test context. This is similar to how you
+// might begin an R test file with 'context()', expect the
+// associated context should be wrapped in braced.
+context("Ordered_Pair template works properly") {
+
+  const auto p12 = Ordered_Pair<int>(1,2);
+  const auto p22 = Ordered_Pair<int>(2,2);
+  const auto p23 = Ordered_Pair<int>(3,2);
+  const auto p13 = Ordered_Pair<int>(3,1);
+
+  test_that("Equality works") {
+    expect_true(p12 == Ordered_Pair<int>(2,1));
+    expect_true(p12 == Ordered_Pair<int>(1,2));
+    expect_false(p12 == p22);
+    expect_false(p12 == p13);
+  }
+
+  test_that("Less Than works"){
+    expect_true(p12 < p22);
+    expect_true(p13 < p22);
+
+    expect_false(p22 < p12);
+    expect_false(p22 < p13);
+  }
+
+}

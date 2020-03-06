243e17c5e9cbad421bdd2be630d5cb519e5dbea6
Nick Strayer <nick.strayer@gmail.com>
2020-03-06 11:59:42 -0600

    Updated Edge container tests to work with new Node container formatting

--- src/test-Edge_Container.cpp
+++ src/test-Edge_Container.cpp
@@ -9,21 +9,21 @@ context("Basic unipartite network") {
   const Rcpp::CharacterVector nodes_id{"a1", "a2", "a3"};
   const Rcpp::CharacterVector nodes_type{"a", "a", "a"};
   const Rcpp::CharacterVector types_name{"a"};
-
+  const Rcpp::IntegerMatrix types_count{3};
 
   // Fully connected network (Except a3, which is not connected to itself)
   const Rcpp::CharacterVector edges_from{"a1", "a1", "a1", "a2", "a2"};
   const Rcpp::CharacterVector   edges_to{"a1", "a2", "a3", "a2", "a3"};
 
   test_that("Sizing is correct") {
-    auto nodes = Node_Container(nodes_id, nodes_type, types_name);
+    auto nodes = Node_Container(nodes_id, nodes_type, types_name, types_count);
     auto edges = Edge_Container(edges_from, edges_to, nodes_id, nodes);
 
     expect_true(edges.size() == 5);
   }
 
   test_that("Edge types were tracked"){
-    auto nodes = Node_Container(nodes_id, nodes_type, types_name);
+    auto nodes = Node_Container(nodes_id, nodes_type, types_name, types_count);
     auto edges = Edge_Container(edges_from, edges_to, nodes_id, nodes);
 
     expect_true(edges.edge_types.size() == 1);
@@ -36,8 +36,9 @@ context("Checking of node correctness") {
   const Rcpp::CharacterVector nodes_id{"a1", "a2", "a3"};
   const Rcpp::CharacterVector nodes_type{"a", "a", "a"};
   const Rcpp::CharacterVector types_name{"a"};
+  const Rcpp::IntegerMatrix types_count{3};
+  auto nodes = Node_Container(nodes_id, nodes_type, types_name, types_count);
 
-  auto nodes = Node_Container(nodes_id, nodes_type, types_name);
 
   // Try to connect a node a4 that wasn't declared in nodes
   const Rcpp::CharacterVector edges_from{"a1", "a1", "a1", "a4"};
@@ -55,47 +56,45 @@ context("Node edges are updated after construction of Edge_Container") {
   const Rcpp::CharacterVector nodes_id{"a1", "a2", "a3"};
   const Rcpp::CharacterVector nodes_type{"a", "a", "a"};
   const Rcpp::CharacterVector types_name{"a"};
+  const Rcpp::IntegerMatrix types_count{3};
 
   // Try to connect a node a4 that wasn't declared in nodes
   const Rcpp::CharacterVector edges_from{"a1", "a1", "a1", "a3"};
   const Rcpp::CharacterVector   edges_to{"a1", "a2", "a3", "a2"};
 
   test_that("Edges update on passed nodes container") {
-    auto nodes = Node_Container(nodes_id, nodes_type, types_name);
+    auto nodes = Node_Container(nodes_id, nodes_type, types_name, types_count);
 
-    expect_true(nodes.at(0).get_degree() == 0); // a1
-    expect_true(nodes.at(1).get_degree() == 0); // a2
-    expect_true(nodes.at(2).get_degree() == 0); // a3
+    expect_true(nodes.at(0, 0).get_degree() == 0); // a1
+    expect_true(nodes.at(0, 1).get_degree() == 0); // a2
+    expect_true(nodes.at(0, 2).get_degree() == 0); // a3
 
     auto edges = Edge_Container(edges_from, edges_to, nodes_id, nodes);
-    expect_true(nodes.at(0).get_degree() == 4); // a1
-    expect_true(nodes.at(1).get_degree() == 2); // a2
-    expect_true(nodes.at(2).get_degree() == 2); // a3
+    expect_true(nodes.at(0, 0).get_degree() == 4); // a1
+    expect_true(nodes.at(0, 1).get_degree() == 2); // a2
+    expect_true(nodes.at(0, 2).get_degree() == 2); // a3
   }
 }
-
 
 context("Basic bipartite network") {
 
   const Rcpp::CharacterVector nodes_id{"a1", "a2", "b1", "b2"};
   const Rcpp::CharacterVector nodes_type{"a", "a", "b", "b"};
   const Rcpp::CharacterVector types_name{"a", "b"};
+  const Rcpp::IntegerMatrix types_count{2, 2};
 
   // Fully connected bipartite network
   const Rcpp::CharacterVector edges_from{"a1", "a1", "a2", "a2"};
   const Rcpp::CharacterVector   edges_to{"b1", "b2", "b1", "b2"};
 
-  auto nodes = Node_Container(nodes_id, nodes_type, types_name);
+  auto nodes = Node_Container(nodes_id, nodes_type, types_name, types_count);
+  auto edges = Edge_Container(edges_from, edges_to, nodes_id, nodes);
 
   test_that("Sizing is correct") {
-    auto edges = Edge_Container(edges_from, edges_to, nodes_id, nodes);
-
     expect_true(edges.size() == 4);
   }
 
   test_that("Edge types were tracked"){
-    auto edges = Edge_Container(edges_from, edges_to, nodes_id, nodes);
-
     expect_true(edges.edge_types.size() == 1);
   }
 
@@ -110,26 +109,24 @@ context("Basic bipartite network") {
   }
 }
 
-
 context("Basic tripartite network") {
   const Rcpp::CharacterVector nodes_id{"a1", "a2", "b1", "c1", "c2"};
   const Rcpp::CharacterVector nodes_type{"a", "a", "b", "c", "c"};
   const Rcpp::CharacterVector types_name{"a", "b", "c"};
+  const Rcpp::IntegerMatrix types_count{  2,   1,   2};
 
   // Only has connections from a-b and a-c
   const Rcpp::CharacterVector edges_from{"a1", "a1", "a2", "a2"};
   const Rcpp::CharacterVector   edges_to{"b1", "c1", "b1", "c2"};
 
-  auto nodes = Node_Container(nodes_id, nodes_type, types_name);
+  auto nodes = Node_Container(nodes_id, nodes_type, types_name, types_count);
+  auto edges = Edge_Container(edges_from, edges_to, nodes_id, nodes);
 
   test_that("Sizing is correct") {
-    auto edges = Edge_Container(edges_from, edges_to, nodes_id, nodes);
     expect_true(edges.size() == 4);
   }
 
   test_that("Edge types were tracked"){
-    auto edges = Edge_Container(edges_from, edges_to, nodes_id, nodes);
-
     expect_true(edges.edge_types.size() == 2);
   }
 
@@ -148,21 +145,20 @@ context("Fully connected tripartite network") {
   const Rcpp::CharacterVector nodes_id{"a1", "a2", "b1", "c1", "c2"};
   const Rcpp::CharacterVector nodes_type{"a", "a", "b", "c", "c"};
   const Rcpp::CharacterVector types_name{"a", "b", "c"};
+  const Rcpp::IntegerMatrix types_count{  2,   1,   2};
 
   // Has connections from a-b, a-c, and b-c
   const Rcpp::CharacterVector edges_from{"a1", "a1", "a2", "a2", "b1", "b1"};
   const Rcpp::CharacterVector   edges_to{"b1", "c1", "b1", "c2", "c2", "c1"};
 
-  auto nodes = Node_Container(nodes_id, nodes_type, types_name);
+  auto nodes = Node_Container(nodes_id, nodes_type, types_name, types_count);
+  auto edges = Edge_Container(edges_from, edges_to, nodes_id, nodes);
 
   test_that("Sizing is correct") {
-    auto edges = Edge_Container(edges_from, edges_to, nodes_id, nodes);
     expect_true(edges.size() == 6);
   }
 
   test_that("Edge types were tracked"){
-    auto edges = Edge_Container(edges_from, edges_to, nodes_id, nodes);
-
     expect_true(edges.edge_types.size() == 3);
   }
 

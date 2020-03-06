4d047a6b71c1328832a8521a741ffe0eb9416d63
Nick Strayer <nick.strayer@gmail.com>
2020-03-06 10:48:54 -0600

    Node_Container now places nodes in unique pointers and sends them to vectors that contain just a single node type

--- src/Node_Container.h
+++ src/Node_Container.h
@@ -3,8 +3,11 @@
 
 // [[Rcpp::plugins(cpp11)]]
 #include <map>
+#include <memory>
 #include <Rcpp.h>
 #include "Node.h"
+
+using namespace Rcpp;
 
 struct Type_Info
 {
@@ -16,125 +19,111 @@ struct Type_Info
   }
 };
 
-using namespace Rcpp;
 
 using Type_Info_Map = std::map<int, Type_Info>;
-using Node_Vec = std::vector<Node>;
+using Node_Unique_Ptr = std::unique_ptr<Node>;
+using Node_Vec = std::vector<Node_Unique_Ptr>;
+using Node_Type_Vec = std::vector<Node_Vec>;
 
 class Node_Container
 {
+private:
+  const void check_for_type(const int type_i) const {
+    if (type_i > nodes.size()) stop("Invalid type");
+  }
 public:
   // Data
-  Node_Vec nodes;          // Same order as in `node_*` inputs.
+  Node_Type_Vec nodes;  // Vector of vectors type->nodes of type ordering
   Type_Info_Map type_info; // Keyed by type_index, returns info about that type in relation to internal nodes vector
   std::map<string, int> type_to_index;
   // Setters
   // ===========================================================================
-  Node_Container(const Rcpp::CharacterVector &nodes_id,
-                 const Rcpp::CharacterVector &nodes_type,
-                 const Rcpp::CharacterVector &types_name)
+  Node_Container(const CharacterVector &nodes_id,
+                 const CharacterVector &nodes_type,
+                 const CharacterVector &types_name,
+                 const IntegerVector   &types_count)
   {
-    const int num_nodes = nodes_id.size();
     const int num_types = types_name.size();
 
-    // Build a map to go from type name to index for faster lookup
+    // Reserve proper number of sub vectors for nodes based on number of types
+    nodes = Node_Type_Vec(num_types);
+
+    // Build a map to go from type name to index for faster look-up
     for (int i = 0; i < num_types; i++)
     {
-      type_to_index.emplace(types_name[i], i);
+      type_to_index.emplace(types_name[i], i);  // Fill in type-to-index map entry for type
+      nodes[i].reserve(types_count[i]);         // Reserve appropriate size for nodes vector for this type
     }
 
-    // Keep track of last seen type (starting at first one)
-    int type_start_index_in_nodes = 0;
-    string current_type = string(nodes_type[0]);
-    int current_type_index = type_to_index.at(current_type);
-
-    for (int i = 0; i < num_nodes; i++)
+    for (int i = 0; i < nodes_id.size(); i++)
     {
+      // Find index for type
+      const auto type_index_it = type_to_index.find(std::string(nodes_type[i]));
 
-      const auto node_type = std::string(nodes_type[i]);
+      // Make sure it fits what we were given
+      if (type_index_it == type_to_index.end())
+        Rcpp::stop("Node " + string(nodes_id[i]) + " has type (" + string(nodes_type[i]) + ") not found in provided node types");
 
-      const bool new_type = current_type != node_type;
-      const bool last_node = i == (num_nodes - 1);
+      // Update the reference to the
+      const int type_index = type_index_it->second;
 
-      // We've finished a type's nodes if we have a new type or its the last node
-      if (new_type | last_node)
-      {
-        const int end_index = last_node ? i : i - 1;
-        const int num_nodes_of_type = (end_index - type_start_index_in_nodes) + 1;
-
-        // Build type_info struct for the just finished class
-        type_info.emplace(current_type_index,
-                          Type_Info(type_start_index_in_nodes, num_nodes_of_type));
-
-        // now we setup for the next type's nodes
-        current_type = node_type;
-        type_start_index_in_nodes = i;
-      }
-
-      if (new_type)
-      {
-        if (current_type > node_type)
-          Rcpp::stop("Nodes not supplied in order. Make sure not to modify the node order");
-
-        // Find index for type
-        const auto type_index_it = type_to_index.find(node_type);
-
-        // Make sure it fits what we were given
-        if (type_index_it == type_to_index.end())
-          Rcpp::stop("Node " + string(nodes_id[i]) + " has type (" + string(nodes_type[i]) + ") not found in provided node types");
-
-        current_type_index = type_index_it->second;
-      }
-
-      // Initialize node in next slot in nodes vector
-      nodes.emplace_back(i, current_type_index);
+      // Build a new node wrapped in smart pointer in it's type vector
+      nodes[type_index].emplace_back(new Node(i, type_index));
     }
-  }
 
+  }
 
   // Getters
   // ===========================================================================
-  Node& at(const int i){
-    if (i < -1 | i > nodes.size()){
-      stop("Invalid node index.");
-    }
-    return nodes[i];
+  // Node& at(const int i){
+  //   if (i < -1 | i > nodes.size()){
+  //     stop("Invalid node index.");
+  //   }
+  //   return nodes[i];
+  // }
+
+  const int size_of_type(const int type_i) const {
+    check_for_type(type_i);
+
+    return nodes[type_i].size();
   }
 
-  Type_Info get_nodes_of_type(const int type_int) const
+  Node_Vec& get_nodes_of_type(const int type_i)
   {
-    return type_info.at(type_int);
+    check_for_type(type_i);
+
+    return nodes[type_i];
+  }
+
+
+  Node& at(const int type_i, const int i){
+
+    Node_Vec& nodes_of_type = get_nodes_of_type(type_i);
+
+    if (i > nodes_of_type.size()){
+      stop("Invalid node index.");
+    }
+
+    return *nodes_of_type[i];
   }
 
   int size() const {
-    return nodes.size();
+    int total_num_nodes = 0;
+    for(const auto& nodes_of_type : nodes){
+      total_num_nodes += nodes_of_type.size();
+    }
+    return total_num_nodes;
   }
 
   int num_types() const {
-    return type_info.size();
+    return nodes.size();
   }
 
   bool is_multipartite() const {
-    return type_info.size() > 1;
+    return num_types() > 1;
   }
 
 
 };
-
-// ## `Node_Container::`
-// __Data__
-// `vec<Node>       nodes`: Vector of `Node`s, same order as in `node_*` inputs.
-// `map<int, Type>  type_info`: unordered_map keyed by a `type_index` that contains a struct with the following info
-//   - `start_index`: Start index in `nodes` for nodes of this type
-//   - `size`: How many nodes there are of type in `nodes`
-
-// __Setters__
-// `Node_Container(nodes_id, nodes_type, types_name, types_count)`: Build a simple node vector one at a time.
-//    - Test type-order assumption while doing so
-//    - At end of a type's set of nodes, build the entry for type in `type_info` map
-// `add_edges(Edge_Container)`: Fill in edges for all nodes by using `Node::add_edge()` method for both sides of edge
-
-// __Getters__
-// `(int, int) get_nodes_of_type(type_int)`: Return pair giving start and end index of type in `nodes` (uses `type_info` map)
 
 #endif

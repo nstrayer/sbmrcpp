7e20efc903844d7628a3354e9ef410ed7fc6a09e
Nick Strayer <nick.strayer@gmail.com>
2020-03-06 15:53:26 -0600

    Added tests for block creation in the tripartite scenario

--- src/test-block_creation.cpp
+++ src/test-block_creation.cpp
@@ -10,7 +10,7 @@ context("Adding blocks for unipartite network") {
   const Rcpp::CharacterVector    nodes_id{"a1", "a2", "a3"};
   const Rcpp::CharacterVector  nodes_type{"a", "a", "a"};
   const Rcpp::CharacterVector  types_name{"a"};
-  const Rcpp::IntegerMatrix   types_count{ 3 };
+  const Rcpp::IntegerVector   types_count{ 3 };
 
   // Initialize a random engine and seed
   Random_Engine random_engine{};
@@ -42,3 +42,54 @@ context("Adding blocks for unipartite network") {
   }
 }
 
+
+
+context("Adding blocks for tripartite network") {
+
+  auto nodes = Node_Container(Rcpp::CharacterVector{"a1", "a2", "a3", "b1", "b2", "c1", "c2", "c3"},
+                              Rcpp::CharacterVector{ "a",  "a",  "a",  "b",  "b",  "c",  "c",  "c"},
+                              Rcpp::CharacterVector{"a", "b", "c"},
+                              Rcpp::IntegerVector{    3,   2,   3});
+
+  // Initialize a random engine and seed
+  Random_Engine random_engine{};
+  random_engine.seed(42);
+
+
+  test_that("Before building blocks, each child node has no parent") {
+    expect_true(nodes.at(0, 0).parent_ref == nullptr); // a1
+    expect_true(nodes.at(0, 1).parent_ref == nullptr); // a2
+    expect_true(nodes.at(0, 2).parent_ref == nullptr); // a3
+    expect_true(nodes.at(1, 0).parent_ref == nullptr); // b1
+    expect_true(nodes.at(1, 1).parent_ref == nullptr); // b2
+    expect_true(nodes.at(2, 0).parent_ref == nullptr); // c1
+    expect_true(nodes.at(2, 1).parent_ref == nullptr); // c2
+    expect_true(nodes.at(2, 2).parent_ref == nullptr); // c3
+  }
+
+  auto blocks = Node_Container(2, nodes, random_engine);
+
+  test_that("Sizing is correct") {
+    expect_true(blocks.size() == 6); // 2 blocks for 3 node types
+  }
+
+  test_that("Each child node now has a parent") {
+    expect_true(nodes.at(0, 0).parent_ref != nullptr); // a1
+    expect_true(nodes.at(0, 1).parent_ref != nullptr); // a2
+    expect_true(nodes.at(0, 2).parent_ref != nullptr); // a3
+    expect_true(nodes.at(1, 0).parent_ref != nullptr); // b1
+    expect_true(nodes.at(1, 1).parent_ref != nullptr); // b2
+    expect_true(nodes.at(2, 0).parent_ref != nullptr); // c1
+    expect_true(nodes.at(2, 1).parent_ref != nullptr); // c2
+    expect_true(nodes.at(2, 2).parent_ref != nullptr); // c3
+  }
+
+  test_that("Each block node has at least one child") {
+    expect_true(blocks.at(0, 0).children.size() > 0);
+    expect_true(blocks.at(0, 1).children.size() > 0);
+    expect_true(blocks.at(1, 0).children.size() > 0);
+    expect_true(blocks.at(1, 1).children.size() > 0);
+    expect_true(blocks.at(2, 0).children.size() > 0);
+    expect_true(blocks.at(2, 1).children.size() > 0);
+  }
+}

238cd75b4e22cd7f688e4192d001f5bea58531d2
Nick Strayer <nick.strayer@gmail.com>
2020-03-10 12:34:43 -0500

    Info about network rearranged to test the move proposal function

--- src/info_about_network.cpp
+++ src/info_about_network.cpp
@@ -1,5 +1,6 @@
 #include <Rcpp.h>
 #include "Node_Container.h"
+#include "Edge_Container.h"
 #include "Ordered_Pair.h"
 
 using namespace Rcpp;
@@ -13,12 +14,33 @@ List info_about_network(const CharacterVector nodes_id,
                         const CharacterVector types_name,
                         const IntegerVector types_count)
 {
+  // Initialize a random engine and seed
+  Random_Engine random_engine{};
+  random_engine.seed(42);
 
   auto nodes = Node_Container(nodes_id, nodes_type, types_name, types_count);
-  // auto edges = Edge_Container(edges_from, edges_to, nodes_id, nodes);
+  auto edges = Edge_Container(edges_from, edges_to, nodes_id, nodes);
+
+  // Assign random blocks
+  auto blocks = Node_Container(5, nodes, random_engine);
+
+  // Get reference to first node in network
+  Node * n1 = &nodes.at(0,0);
+
+  Rcout << "Got node " << n1->get_id(nodes_id) << " with degree of " << n1->get_degree() << std::endl;
+
+  Rcout << "Random neighbor: " << n1->get_random_neighbor(random_engine)->get_id(nodes_id) << std::endl;
+// //
+//   // Get a proposed move for node
+//   Node * proposed_block = propose_move(n1, blocks, random_engine, 0.1);
+
+
+
+
 
   return List::create(_["num_nodes"] = nodes.size(),
                       _["num_types"] = nodes.type_to_index.size()
+                      // _["Random Selection"] = random_selection
                       // // _["num_edges"] = edges.size(),
                       // _["first type start"] = nodes.get_nodes_of_type(0).start_index,
                       // _["first type size"] = nodes.get_nodes_of_type(0).size,
@@ -26,6 +48,7 @@ List info_about_network(const CharacterVector nodes_id,
                       // _["second type size"] = nodes.get_nodes_of_type(1).size
                         );
 }
+
 
 
 
@@ -39,7 +62,9 @@ nodes <- dplyr::bind_rows(dplyr::tibble(id = edges$pollinator,
                                         type = "pollinator"),
                           dplyr::tibble(id = edges$flower,
                                         type = "flower")) %>%
+  dplyr::distinct() %>%
   dplyr::arrange(type)
+
 
 types <- dplyr::count(nodes, type, name = "count")
 

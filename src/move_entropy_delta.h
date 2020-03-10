2103fbb0a5dea2d76a35c9b574bf43c4a769e923
Nick Strayer <nick.strayer@gmail.com>
2020-03-10 16:45:51 -0500

    Changed format so only the new block needs to be passed to the move_entropy_delta function

--- src/move_entropy_delta.h
+++ src/move_entropy_delta.h
@@ -9,11 +9,15 @@ using Edge_Vec = std::vector<Edge>;
 using Edge_Map = std::map<Edge, int>;
 using Edge_Map_Pair = std::pair<Edge, int>;
 
+
+
 double move_entropy_delta(Node* node,
-                          Node* old_block,
                           Node* new_block,
                           Edge_Vec& edges){
 
+  Node* old_block = node->get_parent();
+  // No need to go on if we're "swapping" to the same group
+  if(new_block == old_block) return 0.0;
 
   // Initialize a `pre_move_edge_map` and a `post_move_edge_map`s
   Edge_Map pre_move_edge_counts;
@@ -61,7 +65,6 @@ double move_entropy_delta(Node* node,
     return sum + n_edges * std::log(n_edges/(g1_degree*g2_degree));
   };
 
-
   // Post-move sum
   auto edge_contribution_post_move = [&](double sum, const Edge_Map_Pair& edge_count){
     const Node* g1 = edge_count.first.first();
@@ -92,6 +95,5 @@ double move_entropy_delta(Node* node,
   // Take difference of two sums
   return post_move_edge_entropy - pre_move_edge_entropy;
 }
-
 
 #endif

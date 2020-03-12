5c496059b3b1a485fe1fd9b7ccaa9f679d3c387d
Nick Strayer <nick.strayer@gmail.com>
2020-03-12 17:42:38 -0500

    Updated calling of calc_edge_entropy to use the hard passed new and old block arguments instead of the array initialization

--- src/get_move_results.h
+++ src/get_move_results.h
@@ -40,13 +40,13 @@ Move_Results get_move_results(Node* node,
   const double epsB = eps * double(n_possible_neighbors);
   Node_Edge_Counts node_to_blocks = node->get_block_edge_counts();
 
-  const double pre_move_ent = calc_edge_entropy(nodes, edges, {old_block, new_block});
+  const double pre_move_ent = calc_edge_entropy(nodes, edges, old_block, new_block);
   const double pre_move_prob = calc_move_prob(node_to_blocks, new_block, node_degree, eps, epsB);
 
   // Move node to proposed block
   swap_block(node, new_block, blocks, false);
 
-  const double post_move_ent = calc_edge_entropy(nodes, edges, {old_block, new_block});
+  const double post_move_ent = calc_edge_entropy(nodes, edges, old_block, new_block);
   const double post_move_prob = calc_move_prob(node_to_blocks, old_block, node_degree, eps, epsB);
 
   return Move_Results(pre_move_ent - post_move_ent,

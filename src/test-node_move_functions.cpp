33c7d396bee4cb38d0efe803b905ae536be6e926
Nick Strayer <nick.strayer@gmail.com>
2020-03-12 17:16:13 -0500

    Updated the node move function tests to reflect the new return format of get_move_results()

--- src/test-node_move_functions.cpp
+++ src/test-node_move_functions.cpp
@@ -169,7 +169,7 @@ context("Move entropy delta") {
   Node* g3 = n5->get_parent();
 
   // Propose moving n4 from g2 to g3
-  const double move_delta = get_move_results(n4, g3, nodes, blocks, edges);
+  const double move_delta = get_move_results(n4, g3, nodes, blocks, edges).entropy_delta;
 
   expect_approx_equal(move_delta, -0.1989031);
 
@@ -229,7 +229,7 @@ context("Move proposal returns values are correct (simple unipartite)") {
   // Group c is n5 and n6's parent
   Node* group_c = n5->get_parent();
   // Propose move of n4 to group c
-  const double move_delta = get_move_results(n4, group_c, nodes, blocks, edges);
+  const double move_delta = get_move_results(n4, group_c, nodes, blocks, edges).entropy_delta;
 
   // Delta from hand calculation
   expect_approx_equal(move_delta,  -0.1117765);
@@ -285,7 +285,7 @@ context("Move proposal returns values are correct (simple bipartite)") {
   expect_true(blocks.size() == 6);
 
   // propose moving a2 into block with a1
-  const double move_delta = get_move_results(n_id.at("a2"), n_id.at("a1")->get_parent(), nodes, blocks, edges);
+  const double move_delta = get_move_results(n_id.at("a2"), n_id.at("a1")->get_parent(), nodes, blocks, edges).entropy_delta;
 
   expect_approx_equal(move_delta, -0.5924696);
 

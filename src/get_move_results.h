66152a42617960ae8865d8253e4dd02e03d12916
Nick Strayer <nick.strayer@gmail.com>
2020-03-13 14:36:53 -0500

    Swapped the previous seperate calculation for proposal entropy work to a new version that does not need to move the node or scan the entirety of the edges

--- src/get_move_results.h
+++ src/get_move_results.h
@@ -3,11 +3,17 @@
 // calculates both the entropy delta of the SBM before and after the proposed move and
 // the ratio of the probabilities of moving to the proposed block before the move and
 // moving back to the original block after the move.
-#include "calc_edge_entropy.h"
+// #include "calc_edge_entropy.h"
+#include "Edge_Container.h"
 #include "calc_move_prob.h"
 #include "swap_blocks.h"
 
+using Edge = Ordered_Pair<Node*>;
+using Edge_Map = std::map<Edge, int>;
+using Edge_Map_Pair = std::pair<Edge, int>;
+
 using Node_Edge_Counts = std::map<Node*, int>;
+using Edge_Count = std::pair<Node*, int>;
 
 struct Move_Results {
   double entropy_delta = 0.0;
@@ -16,6 +22,20 @@ struct Move_Results {
     entropy_delta(e),
     prob_ratio(p) {}
 };
+
+void sum_edge_counts(Edge_Map& pair_counts,
+                     Node* this_block,
+                     const Node* to_ignore = nullptr){
+
+  auto add_edge = [&](const Edge_Count& block_count){
+    if (block_count.first != to_ignore)
+      pair_counts[Edge(this_block, block_count.first)] += block_count.second;
+  };
+
+  Node_Edge_Counts edges_cts = this_block->get_block_edge_counts();
+
+  std::for_each(edges_cts.begin(), edges_cts.end(), add_edge);
+}
 
 Move_Results get_move_results(Node* node,
                               Node* new_block,
@@ -41,13 +61,71 @@ Move_Results get_move_results(Node* node,
 
   Node_Edge_Counts node_to_blocks = node->get_block_edge_counts();
 
-  const double pre_move_ent = calc_edge_entropy(nodes, edges, old_block, new_block);
-  const double prob_move_to_new = calc_move_prob(node_to_blocks, new_block, node_degree, eps, epsB);
+  Edge_Map block_pair_counts;
+  sum_edge_counts(block_pair_counts, old_block, new_block);
+  sum_edge_counts(block_pair_counts, new_block);
+  bool post_move = false;
 
+  auto calc_edge_entropy_part = [&post_move, &old_block, &new_block, &node_degree]
+                                (double ent_sum, const Edge_Map_Pair& edge_count){
+    const double n_edges = edge_count.second;
+    if(n_edges == 0.0) return ent_sum;
+
+    // Self edge contributions need their edge counts doubled (because they are half edges)
+    // and also need to have their total contribution divided by two
+    // because they are getting counted twice as "frequently as the off-diagonal pairs
+    const Edge& edge = edge_count.first;
+    const double edge_scalar = edge.is_matching() ? 2.0: 1.0;
+
+    const Node* g1 = edge.first();
+    const Node* g2 = edge.second();
+
+    double g1_degree = g1->get_degree();
+    double g2_degree = g2->get_degree();
+
+    if(post_move){
+      if (g1 == old_block) {
+        g1_degree -= node_degree;
+      } else
+      if (g1 == new_block) {
+        g1_degree += node_degree;
+      }
+
+      if (g2 == old_block) {
+        g2_degree -= node_degree;
+      } else
+      if (g2 == new_block) {
+        g2_degree += node_degree;
+      }
+    }
+
+    return ent_sum + n_edges * std::log(n_edges/(g1_degree*g2_degree)) / edge_scalar;;
+  };
+
+  const double pre_move_ent = std::accumulate(block_pair_counts.begin(),
+                                              block_pair_counts.end(),
+                                              0.0,
+                                              calc_edge_entropy_part);
+
+  // Update edge counts
+  for (const auto& block_count : node_to_blocks) {
+    // subtract contributions for when node was in old block
+    block_pair_counts[Edge(old_block, block_count.first)] -= block_count.second * (block_count.first == old_block ? 2 : 1);
+
+    // add edge to new blocks edge counts
+    block_pair_counts[Edge(new_block, block_count.first)] += block_count.second * (block_count.first == new_block ? 2 : 1);
+  }
+
+  post_move = true;
+  const double post_move_ent = std::accumulate(block_pair_counts.begin(),
+                                               block_pair_counts.end(),
+                                               0.0,
+                                               calc_edge_entropy_part);
+
+
+  const double prob_move_to_new = calc_move_prob(node_to_blocks, new_block, node_degree, eps, epsB);
   // Move node to proposed block
   swap_block(node, new_block, blocks, false);
-
-  const double post_move_ent = calc_edge_entropy(nodes, edges, old_block, new_block);
   const double prob_return_to_old = calc_move_prob(node_to_blocks, old_block, node_degree, eps, epsB);
 
   // Return node to original block
